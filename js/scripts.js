adbb0bc14d8182fb11ba3fa9b5a786c9d7c5158f
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-09-11 11:56:22 +0300

    Add a toggle function on what we do section.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".shown").click(function(){
+        $(this).toggle();
+        $(this).siblings().toggle();
+    });
+    $(".hidden").click(function(){
+        $(this).siblings().toggle();
+        $(this).toggle();
+    });
+});
