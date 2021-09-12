dcc84a225470ad48cc231595e4a5953f7a91f428
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-09-12 12:11:32 +0300

    Add a function for form action and validation. Also with alert message.

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,27 @@ $(document).ready(function(){
         $(this).find(".whenHover").hide();
     });
 });
+
+
+
+function sendMessage() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    
+    if(name.length === 0){
+        alert("Enter your name!");
+        break;
+    }
+    else if(email.length === 0){
+        alert("Enter your email!");
+        break;
+    }
+    else if(message.length === 0){
+        alert("Write your message!");
+        break;
+    }
+    else{
+        alert("Hey" + name + ", We have receiived your message. Thank you for reaching out to us.")
+    }
+}
